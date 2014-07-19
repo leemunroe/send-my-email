3de76e650620097bcbe41742da7328eb449a8d07
Lee Munroe <lee@leemunroe.com>
2014-07-18 18:05:04 -0700

    Change port

--- app.js
+++ app.js
@@ -49,5 +49,7 @@ app.post('/', function(req, res) {
 
 });
 
-module.exports = app;
-
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
